3305a6cd71ba51050bd945d961463de80ed7dd9a
mammadli <gulnar.mammadli@ut.ee>
2023-05-01 20:50:01 +0300

    Change question parameters

--- src/main/java/org/mammadli/Main.java
+++ src/main/java/org/mammadli/Main.java
@@ -14,35 +14,32 @@ public class Main {
         quiz.setId(1);
 
         Question question1 = new Question();
-        question1.setId(2);
+        question1.setId(1);
         question1.setContent("What is the capital of France?");
         question1.setTopic("Geography");
         question1.setDifficultyRank(3);
         List<Response> responses = new ArrayList<>();
-        responses.add(new Response(4, "Paris", true));
-        responses.add(new Response(5, "London", false));
-        responses.add(new Response(6, "Rome", false));
-        responses.add(new Response(7, "Madrid", false));
+        responses.add(new Response(1, "Paris", true));
+        responses.add(new Response(2, "London", false));
         question1.setResponses(responses);
 
 
         Question newQuestion = new Question();
-        newQuestion.setId(2);
+        newQuestion.setId(1);
         newQuestion.setContent("What is the capital of Estonia?");
         newQuestion.setTopic("Geography");
         newQuestion.setDifficultyRank(7);
         List<Response> resp = new ArrayList<>();
-        resp.add(new Response(4, "Paris", true));
-        resp.add(new Response(5, "London", false));
-        resp.add(new Response(6, "Rome", false));
-        resp.add(new Response(7, "Madrid", false));
+        resp.add(new Response(1, "Tallinn", true));
+        resp.add(new Response(2, "London", false));
         newQuestion.setResponses(resp);
 
-//        daoQuestion.saveQuiz(quiz);
-//        daoQuestion.deleteQuestion(4);
-//        daoQuestion.saveQuestion(question1, quiz);
-//        daoQuestion.searchQuestionsByTopic("Geography");
-//        daoQuestion.updateQuestion(2, newQuestion);
+
+        daoQuestion.saveQuiz(quiz);
+        daoQuestion.deleteQuestion(1);
+        daoQuestion.saveQuestion(question1, quiz);
+        daoQuestion.searchQuestionsByTopic("Geography");
+        daoQuestion.updateQuestion(1, newQuestion);
 
     }
 }
